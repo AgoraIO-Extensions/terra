ab4060c5489e324b02c5ac2d6650e83b1f059de8
littleGnAl <tearjeaker@hotmail.com>
2023-09-14 23:39:03 +0800

    Refactor Parser & Renderer

--- terra/src/generators/default_generator.ts
+++ terra/src/generators/default_generator.ts
@@ -1,9 +1,9 @@
-import { Generator, ParseResult } from '@agoraio-extensions/terra-core';
+// import { Generator, ParseResult } from '@agoraio-extensions/terra-core';
 
-export class DefaultGenerator implements Generator {
-  generate(parseResult: ParseResult): boolean {
-    // Do nothing at this time.
-    console.log(parseResult);
-    return true;
-  }
-}
+// export class DefaultGenerator implements Generator {
+//   generate(parseResult: ParseResult): boolean {
+//     // Do nothing at this time.
+//     console.log(parseResult);
+//     return true;
+//   }
+// }
