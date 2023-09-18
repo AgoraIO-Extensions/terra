f59a92cc7f134d83dd8f55903c99e207e15da6c3
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2023-09-18 14:41:11 +0800

    fix: Fix build error (#4)

--- cxx-parser/src/cxx_parser_ext.ts
+++ cxx-parser/src/cxx_parser_ext.ts
@@ -30,17 +30,17 @@ ParseResult.prototype.findClazz = function (
   const clazzNameNamespace = clazzName.getNamespace();
   const trimNamespacesName = clazzName.trimNamespace();
 
-  for (const node of this.nodes) {
-    let cxxFile = node as CXXFile;
+  for (const f of this.nodes) {
+    let cxxFile = f as CXXFile;
     for (const node of cxxFile.nodes) {
       if (node.__TYPE == CXXTYPE.Clazz) {
         const clazz = node;
 
         if (clazz.name == trimNamespacesName) {
           if (
-            clazz.getNamespacesInString() == clazzNameNamespace ||
+            clazz.namespace == clazzNameNamespace ||
             clazzNameNamespace == "" ||
-            clazz.getNamespacesInString().includes(clazzNameNamespace)
+            clazz.namespace.includes(clazzNameNamespace)
           ) {
             return clazz as Clazz;
           }
@@ -66,17 +66,17 @@ ParseResult.prototype.findEnumz = function (
 
   const trim_namespaces_name = enumzName.trimNamespace();
 
-  for (const n of this.nodes) {
-    let cxx_file = n as CXXFile;
-    for (const node of cxx_file.nodes) {
+  for (const f of this.nodes) {
+    let cxxFile = f as CXXFile;
+    for (const node of cxxFile.nodes) {
       if (node.__TYPE == CXXTYPE.Enumz) {
         const enumz = node as Enumz;
 
         if (enumz.name === trim_namespaces_name) {
           if (
-            enumz.getNamespacesInString() == namespace_string ||
+            enumz.namespace == namespace_string ||
             namespace_string == "" ||
-            enumz.getNamespacesInString().includes(namespace_string)
+            enumz.namespace.includes(namespace_string)
           ) {
             return enumz;
           }
---
+++ terra/__tests__/unit_test/renderers/dump_json_renderer.test.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+import { ParseResult, TerraContext } from '@agoraio-extensions/terra-core';
+
+import { dumpJsonRenderer, terraAstJsonFileName } from './dump_json_renderer';
+
+interface FakeNode {
+  name: string;
+}
+
+describe('dumpJsonRenderer', () => {
+  it('able to dump terra ast json', () => {
+    let tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'terra-ut-'));
+
+    let dumpJsonFunc = dumpJsonRenderer(
+      new TerraContext('', tmpDir, false, false)
+    );
+    let terraAstPath = path.join(tmpDir, terraAstJsonFileName);
+
+    let parseResult = new ParseResult();
+    let fakeNode1: FakeNode = { name: 'fakeNode1' };
+    let fakeNode2: FakeNode = { name: 'fakeNode2' };
+    parseResult.nodes = [fakeNode1, fakeNode2];
+
+    let expectedContent = JSON.stringify(parseResult.nodes);
+
+    dumpJsonFunc({} as TerraContext, {}, parseResult);
+
+    expect(fs.readFileSync(terraAstPath, 'utf8')).toEqual(expectedContent);
+  });
+});
