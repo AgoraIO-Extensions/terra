14316d722eb4293f0e786169368c19f5a6c90516
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2023-09-21 21:12:02 +0800

    chore: jest config (#16)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,25 @@
 module.exports = {
-  coverageDirectory: 'coverage',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '/__tests__/.*\\.(test|spec)\\.[jt]sx?$',
+  projects: [
+    {
+      displayName: 'cxx-parser',
+      coverageDirectory: 'cxx-parser',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testRegex: 'cxx-parser/__tests__/.*\\.(test|spec)\\.[jt]sx?$',
+    },
+    {
+      displayName: 'terra',
+      coverageDirectory: 'terra',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testRegex: 'terra/__tests__/.*\\.(test|spec)\\.[jt]sx?$',
+    },
+    {
+      displayName: 'terra-core',
+      coverageDirectory: 'terra-core',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testRegex: 'terra-core/__tests__/.*\\.(test|spec)\\.[jt]sx?$',
+    },
+  ],
 };
