88a1cf16708a5859fb8dda3d197fa3bb65b8f1c8
littleGnAl <tearjeaker@hotmail.com>
2023-12-19 14:55:02 +0800

    Enhance README.md

---
+++ cxx-parser/README.md
@@ -0,0 +1,11 @@
+# cxx-parser
+
+The `cxx-parser`` is a terra parser that parse C/C++ Abstract Syntax Trees (ASTs), leveraging libraries from [cppast](https://github.com/foonathan/cppast) and [clang AST](https://clang.llvm.org/docs/IntroductionToTheClangAST.html).
+
+## Prerequisites
+> The [cppast](https://github.com/foonathan/cppast) only work on llvm@15 at this time, you can install it using homebrew
+> ```
+> brew install llvm@15
+> ```
+
+- Install llvm@15
--- terra/src/base/terra_configs.ts
+++ terra/src/base/terra_configs.ts
@@ -4,16 +4,48 @@ import { resolvePath } from '@agoraio-extensions/terra-core';
 
 import YAML from 'yaml';
 
+/**
+ * Configuration settings for a terra Parser or Renderer.
+ */
 export interface TerraLoaderConfig {
+  /**
+   * The name of the Parser or Renderer. Optional if the Parser or Renderer is defined
+   * as a default export.
+   */
   name?: string;
+  /**
+   * The package name where the Parser or Renderer is located.
+   * Note: Specify either `package` or `path`, but not both.
+   */
   package?: string;
+  /**
+   * The relative file system path to the parser or renderer, relative to
+   * current configuration path.
+   * Note: Specify either `path` or `package`, but not both.
+   */
   path?: string;
+  /**
+   * Arguments to be passed to the Parser or Renderer.
+   * Can be of any type.
+   */
   args?: any;
 }
 
+/**
+ * Configuration for terra, including parsers and renderers.
+ */
 export interface TerraConfigs {
+  /**
+   * Path to the base configuration file for terra, which will be merged with the current configuration, but the same configurations in the current configuration will be overrided the base configuration if the keys are same.
+   */
   include: string;
+  /**
+   * List of parser configurations to be used in terra.
+   */
   parsers: Array<TerraLoaderConfig>;
+  /**
+   * List of renderer configurations to be used in terra.
+   */
   renderers: Array<TerraLoaderConfig>;
 }
 
