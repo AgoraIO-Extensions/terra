b6d01902f1082a83ab77310a30d35a6c87f48297
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-03-07 11:39:27 +0800

    feat: Add some extension functions for CXXTerraNode (#51)
    Add some extension functions to check the node type
    ```
    let node = new CXXFile();
    // before 
    if (node.__TYPE === CXXTYPE.CXXFile) {}
    // now
    if (node.isCXXFile()) {}
    ```

--- cxx-parser/src/cxx_terra_node_ext.ts
+++ cxx-parser/src/cxx_terra_node_ext.ts
@@ -1,10 +1,77 @@
 import { strict as assert } from 'assert';
 
-import { SimpleType, SimpleTypeKind } from './cxx_terra_node';
+import {
+  CXXTYPE,
+  CXXTerraNode,
+  SimpleType,
+  SimpleTypeKind,
+} from './cxx_terra_node';
 
 export {};
 
 declare module '@agoraio-extensions/cxx-parser' {
+  export interface CXXTerraNode {
+    /**
+     * Checks if this node is a `CXXFile`.
+     */
+    isCXXFile(): boolean;
+
+    /**
+     * Checks if this node is an `IncludeDirective`.
+     */
+    isIncludeDirective(): boolean;
+
+    /**
+     * Checks if this node is a `TypeAlias`.
+     */
+    isTypeAlias(): boolean;
+
+    /**
+     * Checks if this node is a `Clazz`.
+     */
+    isClazz(): boolean;
+
+    /**
+     * Checks if this node is a `Struct`.
+     */
+    isStruct(): boolean;
+
+    /**
+     * Checks if this node is a `Constructor`.
+     */
+    isConstructor(): boolean;
+
+    /**
+     * Checks if this node is a `MemberFunction`.
+     */
+    isMemberFunction(): boolean;
+
+    /**
+     * Checks if this node is a `Variable`.
+     */
+    isVariable(): boolean;
+
+    /**
+     * Checks if this node is a `SimpleType`.
+     */
+    isSimpleType(): boolean;
+
+    /**
+     * Checks if this node is a `MemberVariable`.
+     */
+    isMemberVariable(): boolean;
+
+    /**
+     * Checks if this node is a `EnumConstant`.
+     */
+    isEnumConstant(): boolean;
+
+    /**
+     * Checks if this node is a `Enumz`.
+     */
+    isEnumz(): boolean;
+  }
+
   export interface SimpleType {
     /**
      * The length of the array type. For example, "int[10]" returns "10".
@@ -64,4 +131,52 @@ SimpleType.prototype.lenOfArrayType = function (): string {
   }
 
   assert(false); // Should not reach here
+};
+
+CXXTerraNode.prototype.isCXXFile = function (): boolean {
+  return this.__TYPE === CXXTYPE.CXXFile;
+};
+
+CXXTerraNode.prototype.isIncludeDirective = function (): boolean {
+  return this.__TYPE === CXXTYPE.IncludeDirective;
+};
+
+CXXTerraNode.prototype.isTypeAlias = function (): boolean {
+  return this.__TYPE === CXXTYPE.TypeAlias;
+};
+
+CXXTerraNode.prototype.isClazz = function (): boolean {
+  return this.__TYPE === CXXTYPE.Clazz;
+};
+
+CXXTerraNode.prototype.isStruct = function (): boolean {
+  return this.__TYPE === CXXTYPE.Struct;
+};
+
+CXXTerraNode.prototype.isConstructor = function (): boolean {
+  return this.__TYPE === CXXTYPE.Constructor;
+};
+
+CXXTerraNode.prototype.isMemberFunction = function (): boolean {
+  return this.__TYPE === CXXTYPE.MemberFunction;
+};
+
+CXXTerraNode.prototype.isVariable = function (): boolean {
+  return this.__TYPE === CXXTYPE.Variable;
+};
+
+CXXTerraNode.prototype.isSimpleType = function (): boolean {
+  return this.__TYPE === CXXTYPE.SimpleType;
+};
+
+CXXTerraNode.prototype.isMemberVariable = function (): boolean {
+  return this.__TYPE === CXXTYPE.MemberVariable;
+};
+
+CXXTerraNode.prototype.isEnumConstant = function (): boolean {
+  return this.__TYPE === CXXTYPE.EnumConstant;
+};
+
+CXXTerraNode.prototype.isEnumz = function (): boolean {
+  return this.__TYPE === CXXTYPE.Enumz;
 };
