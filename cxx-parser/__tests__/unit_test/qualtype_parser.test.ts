31406cc93e7bfd6f18fe88844feeaa6711a7d51e
WinterPu <striveant@outllook.com>
2025-02-11 18:07:17 +0800

    Update qualtype_parser.test.ts

--- cxx-parser/__tests__/unit_test/qualtype_parser.test.ts
+++ cxx-parser/__tests__/unit_test/qualtype_parser.test.ts
@@ -6,7 +6,9 @@ import path from 'path';
 
 import { ParseResult } from '@agoraio-extensions/terra-core';
 
-import { genParseResultFromJson } from '../../src/cxx_parser';
+import { TerraContext } from '@agoraio-extensions/terra-core';
+
+import { dumpCXXAstJson, genParseResultFromJson } from '../../src/cxx_parser';
 import { CXXFile, CXXTYPE } from '../../src/cxx_terra_node';
 import { ClangASTQualTypeParser } from '../../src/qualtype_parser';
 
@@ -44,102 +46,108 @@ namespace rtc {
 `
     );
 
-    // 准备解析结果
-    const cppastJSON = JSON.stringify([
-      {
-        __TYPE: 'CXXFile',
-        file_path: filePath,
-        nodes: [
-          {
-            __TYPE: 'Clazz',
-            name: 'IRtcEngine',
-            namespaces: ['agora', 'rtc'],
-            methods: [
-              {
-                __TYPE: 'MemberFunction',
-                name: 'joinChannel',
-                mangled_name: '__ZN5agora3rtc10IRtcEngine11joinChannelEj',
-                parameters: [
-                  {
-                    __TYPE: 'Variable',
-                    name: 'uid',
-                    parent_full_scope_name:
-                      'agora::rtc::IRtcEngine::joinChannel',
-                    type: {
-                      __TYPE: 'SimpleType',
-                      is_builtin_type: false,
-                      is_const: false,
-                      kind: 101,
-                      name: 'uid',
-                      source: 'uid_t',
-                      template_arguments: [],
-                      clang_qualtype: '',
-                    },
-                  },
-                ],
-                parent_name: 'IRtcEngine',
-                namespaces: ['agora', 'rtc'],
-              },
-              {
-                __TYPE: 'MemberFunction',
-                name: 'setConfig',
-                mangled_name: '__ZN5agora3rtc10IRtcEngine9setConfigEidPPv',
-                parameters: [
-                  {
-                    __TYPE: 'Variable',
-                    name: 'width',
-                    parent_full_scope_name: 'agora::rtc::IRtcEngine::setConfig',
-                    type: {
-                      __TYPE: 'SimpleType',
-                      is_builtin_type: true,
-                      is_const: false,
-                      kind: 100,
-                      name: 'int',
-                      source: 'int',
-                      template_arguments: [],
-                      clang_qualtype: '',
-                    },
-                  },
-                  {
-                    __TYPE: 'Variable',
-                    name: 'height',
-                    parent_full_scope_name: 'agora::rtc::IRtcEngine::setConfig',
-                    type: {
-                      __TYPE: 'SimpleType',
-                      is_builtin_type: true,
-                      is_const: false,
-                      kind: 100,
-                      name: 'double',
-                      source: 'double',
-                      template_arguments: [],
-                      clang_qualtype: '',
-                    },
-                  },
-                  {
-                    __TYPE: 'Variable',
-                    name: 'view',
-                    parent_full_scope_name: 'agora::rtc::IRtcEngine::setConfig',
-                    type: {
-                      __TYPE: 'SimpleType',
-                      is_builtin_type: false,
-                      is_const: false,
-                      kind: 101,
-                      name: 'view',
-                      source: 'view_t*',
-                      template_arguments: [],
-                      clang_qualtype: '',
-                    },
-                  },
-                ],
-                parent_name: 'IRtcEngine',
-                namespaces: ['agora', 'rtc'],
-              },
-            ],
-          },
-        ],
-      },
-    ]);
+    // // 准备解析结果
+    // const cppastJSON = JSON.stringify([
+    //   {
+    //     __TYPE: 'CXXFile',
+    //     file_path: filePath,
+    //     nodes: [
+    //       {
+    //         __TYPE: 'Clazz',
+    //         name: 'IRtcEngine',
+    //         namespaces: ['agora', 'rtc'],
+    //         methods: [
+    //           {
+    //             __TYPE: 'MemberFunction',
+    //             name: 'joinChannel',
+    //             mangled_name: '__ZN5agora3rtc10IRtcEngine11joinChannelEj',
+    //             parameters: [
+    //               {
+    //                 __TYPE: 'Variable',
+    //                 name: 'uid',
+    //                 parent_full_scope_name:
+    //                   'agora::rtc::IRtcEngine::joinChannel',
+    //                 type: {
+    //                   __TYPE: 'SimpleType',
+    //                   is_builtin_type: false,
+    //                   is_const: false,
+    //                   kind: 101,
+    //                   name: 'uid',
+    //                   source: 'uid_t',
+    //                   template_arguments: [],
+    //                   clang_qualtype: '',
+    //                 },
+    //               },
+    //             ],
+    //             parent_name: 'IRtcEngine',
+    //             namespaces: ['agora', 'rtc'],
+    //           },
+    //           {
+    //             __TYPE: 'MemberFunction',
+    //             name: 'setConfig',
+    //             mangled_name: '__ZN5agora3rtc10IRtcEngine9setConfigEidPPv',
+    //             parameters: [
+    //               {
+    //                 __TYPE: 'Variable',
+    //                 name: 'width',
+    //                 parent_full_scope_name: 'agora::rtc::IRtcEngine::setConfig',
+    //                 type: {
+    //                   __TYPE: 'SimpleType',
+    //                   is_builtin_type: true,
+    //                   is_const: false,
+    //                   kind: 100,
+    //                   name: 'int',
+    //                   source: 'int',
+    //                   template_arguments: [],
+    //                   clang_qualtype: '',
+    //                 },
+    //               },
+    //               {
+    //                 __TYPE: 'Variable',
+    //                 name: 'height',
+    //                 parent_full_scope_name: 'agora::rtc::IRtcEngine::setConfig',
+    //                 type: {
+    //                   __TYPE: 'SimpleType',
+    //                   is_builtin_type: true,
+    //                   is_const: false,
+    //                   kind: 100,
+    //                   name: 'double',
+    //                   source: 'double',
+    //                   template_arguments: [],
+    //                   clang_qualtype: '',
+    //                 },
+    //               },
+    //               {
+    //                 __TYPE: 'Variable',
+    //                 name: 'view',
+    //                 parent_full_scope_name: 'agora::rtc::IRtcEngine::setConfig',
+    //                 type: {
+    //                   __TYPE: 'SimpleType',
+    //                   is_builtin_type: false,
+    //                   is_const: false,
+    //                   kind: 101,
+    //                   name: 'view',
+    //                   source: 'view_t*',
+    //                   template_arguments: [],
+    //                   clang_qualtype: '',
+    //                 },
+    //               },
+    //             ],
+    //             parent_name: 'IRtcEngine',
+    //             namespaces: ['agora', 'rtc'],
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // ]);
 
+    let cppastJSON = dumpCXXAstJson(
+      new TerraContext(tmpDir),
+      [],
+      [filePath],
+      []
+    );
     let parseResult = genParseResultFromJson(cppastJSON);
 
     // 执行解析
@@ -147,15 +155,26 @@ namespace rtc {
 
     // 验证结果
     const cxxFile = parseResult.nodes[0] as CXXFile;
-    const clazz = cxxFile.nodes[0];
-    const methods = clazz.asClazz().methods;
+    cxxFile.nodes.forEach((node) => {
+      if (node.__TYPE == CXXTYPE.Clazz) {
+        const methods = node.asClazz().methods;
 
-    // 验证第一个方法的参数
-    expect(methods[0].parameters[0].type.clang_qualtype).toBe('uid_t');
-
-    // 验证第二个方法的参数
-    expect(methods[1].parameters[0].type.clang_qualtype).toBe('int');
-    expect(methods[1].parameters[1].type.clang_qualtype).toBe('double');
-    expect(methods[1].parameters[2].type.clang_qualtype).toBe('view_t *');
+        methods.forEach((method) => {
+          if (method.name == 'joinChannel') {
+            expect(method.parameters[0].type.clang_qualtype).toBe('uid_t');
+          } else if (method.name == 'setConfig') {
+            method.parameters.forEach((param) => {
+              if (param.name == 'width') {
+                expect(param.type.clang_qualtype).toBe('int');
+              } else if (param.name == 'height') {
+                expect(param.type.clang_qualtype).toBe('double');
+              } else if (param.name == 'view') {
+                expect(param.type.clang_qualtype).toBe('view_t *');
+              }
+            });
+          }
+        });
+      }
+    });
   });
 });
